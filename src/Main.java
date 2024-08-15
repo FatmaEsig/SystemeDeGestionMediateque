01b1a9aedb2cd9f7d475bfd7a832a03bfaaf8010
FatmaEsig <fatma-aydin06@hotmail.com>
2024-08-15 18:02:29 +0200

    possible instructions for users added in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(" ");
+        System.out.println("======= Mediathèque ========");
+        System.out.println(" ");
+        System.out.println("----------------------------");
+
+        // Il faut interaction avec utilisateur, scanner ???
+
+        /* Instructions possibles
+
+        Instruction 1 (qui se pose au démarrage:
+
+        - Pour lister tous nos médias, ecrivez "lister"
+        - Pour lister que tous les livres, ecrivez "livres"
+        - Pour lister que tout les videos, ecrivez "videos"
+
+        - Pour lister tous les médias disponibles, écrivez "disponibles"
+        (pour lister tout les livres disponible, pour lister tout les video disponible : livres disponible, videos disponibles)
+
+        Instruction 2
+
+        - Pour emprunter un livre, ecrivez le nom du livre + emprunter.
+
+        - Pour retourner un livre, ecrivez le nom du livre + retourner.
+
+        --
+
+        - Pour emprunter un video, ecrivez le nom du video + emprunter.
+
+        - Pour retourner un video, ecrivez le nom du video + retourner.
+
+
+        Instruction 3
+
+        - Malheureusement le livre que vous souhaitiez emprunté est actuellement pas disponible, mais vous pouvez
+        le réserver. Ecrivez "Reserver" pour réserver.
+
+        - Vous etes ajouté à la liste d'attente pour ce media : nomMedia, date de reservation : date d'ajourdhui
+
+        - Le media choisi est disponible, maintenant vous pouvez le posseder. La date de retour est  ...
+        (rappel : pour retourner le media emprunté, ecrivez le nom du media + retourner
+
+
+
+        */
+        
+
 
 	// Fabrique du Media à appeler
 		// Strategie à utiliser dans Fabrique , je dois voir comment le faire
